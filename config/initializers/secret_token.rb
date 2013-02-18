07041d3c870fa4449f1e14958f585011b9d6a878
Matthew Gould <matt@curebit.com>
2013-02-18 08:36:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyLanding::Application.config.secret_token = '598f003aee6a5bb624b2e86989af707c8b33150b4d6d340a677d0492fbf10c22718e80522d83a90564d0ef2e0dd5a920ac268f2e1f90c48e2ac76ccd0090f5f7'
